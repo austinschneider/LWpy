d9e11002cb65db7708b2c367e10028edc4cb156b
Austin Schneider <hogenshpogen@gmail.com>
2020-09-19 16:31:00 -0400

    Add setup

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='LWpy',
+      version='0.1',
+      description='Module for weighting LeptonInjector simulation',
+      url='https://github.com/austinschneider/LWpy',
+      author='Austin Schneider',
+      author_email='physics.schneider@gmail.com',
+      license='L-GPL-2.1',
+      packages=[],
+      zip_safe=False)
