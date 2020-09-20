815811ce5a8acdfab1bbfc30273bd73c0a99129b
Austin Schneider <hogenshpogen@gmail.com>
2020-09-20 09:20:19 -0400

    Fix ranged generator issues

--- LWpy/generator/ranged_generator.py
+++ LWpy/generator/ranged_generator.py
@@ -16,12 +16,11 @@ class ranged_generator(generator):
         return (direction*(origin - position)) * direction + position
 
     @staticmethod
-    @np.vectorize
     def GetLeptonRange(energy,
-                      isTau=False,
-                      option=EarthModelService.EarthModelCalculator.LeptonRangeOption.DEFAULT,
-                      scale=1.):
-        return EarthModelService.EarthModelCalculator.GetLeptonRange(energy, isTau, option, scale)
+            isTau=False,
+            option=EarthModelService.EarthModelCalculator.LeptonRangeOption.DEFAULT,
+            scale=1.):
+        return np.array([EarthModelService.EarthModelCalculator.GetLeptonRange(e, isTau, option, scale) for e in energy])
 
     @staticmethod
     @np.vectorize
@@ -33,19 +32,58 @@ class ranged_generator(generator):
     def ColumnDepthCGStoMWE(cdep_CGS):
         return EarthModelService.EarthModelCalculator.ColumnDepthCGStoMWE(cdep_CGS)
 
-    @staticmethod
-    @np.vectorize
-    def GetAtmoPoints(pca, direction):
-        atmoEntry = LeptonInjector.LI_Position()
-        atmoExit = LeptonInjector.LI_Position()
-        EarthModelService.EarthModelCalculator.GetIntersectionsWithSphere(
-                earthModel.GetEarthCoordPosFromDetCoordPos(pca),
-                earthModel.GetEarthCoordDirFromDetCoordDir(direction),
-                earthModel.GetAtmoRadius(), atmoEntry, atmoExit)
+    def GetAtmoPoints(self, pca, direction):
+        a_entry = []
+        a_exit = []
+        for pca, direction in zip(pca, direction):
+            atmoEntry = None
+            atmoExit = None
+            atmoEntry = LeptonInjector.LI_Position()
+            atmoExit = LeptonInjector.LI_Position()
+            EarthModelService.EarthModelCalculator.GetIntersectionsWithSphere(
+                    self.earthModel.GetEarthCoordPosFromDetCoordPos(pca),
+                    self.earthModel.GetEarthCoordDirFromDetCoordDir(direction),
+                    self.earthModel.GetAtmoRadius(), atmoEntry, atmoExit)
 
-        atmoEntry = earthModel.GetDetCoordPosFromEarthCoordPos(atmoEntry)
-        atmoExit = earthModel.GetDetCoordPosFromEarthCoordPos(atmoExit)
-        return atmoEntry, atmoExit
+            atmoEntry = self.earthModel.GetDetCoordPosFromEarthCoordPos(atmoEntry)
+            atmoExit = self.earthModel.GetDetCoordPosFromEarthCoordPos(atmoExit)
+            a_entry.append(atmoEntry)
+            a_exit.append(atmoExit)
+        return np.array(a_entry), np.array(a_exit)
+
+    def GetColumnDepthInCGS(self, p0, p1, use_electron_density=False):
+        return np.array([self.earthModel.GetColumnDepthInCGS(
+            pp0,
+            pp1,
+            use_electron_density)
+            for pp0,pp1 in zip(p0, p1)])
+
+
+    def DistanceForColumnDepthToPoint(self, p0, d0, col, use_electron_density=False):
+        return np.array([self.earthModel.DistanceForColumnDepthToPoint(
+            p,
+            d,
+            c,
+            use_electron_density)
+            for p,d,c in zip(p0, d0, col)])
+    def GetEarthDensitySegments(self, first_point, last_point, use_electron_density=False):
+        return [self.earthModel.GetEarthDensitySegments(fp, lp, use_electron_density) for fp,lp in zip(first_point, last_point)]
+
+    def GetEarthDensityInCGS(self, position):
+        return np.array([self.earthModel.GetEarthDensityInCGS(p) for p in position])
+
+    def GetPNERatio(self, position):
+        proton = int(LeptonInjector.Particle.ParticleType.PPlus)
+        offsets = []
+        for p in position:
+            current_medium = self.earthModel.GetEarthParam(p)
+            offsets.append(self.earthModel.GetPNERatio(current_medium.fMediumType_, proton))
+        return np.array(offsets)
+
+    # double GetPNERatio(MediumType med, int id) const;
+    # double GetPNERatio(MediumType med, int id) const;
+    # density_offset =  GetPNERatio(curMedium.fMediumType_ , 2212);
+    # EarthParam curMedium=GetEarthParam(pos);
 
     def prob_area(self, events):
         events = np.asarray(events)
@@ -55,60 +93,93 @@ class ranged_generator(generator):
         p_area /= 1e4 # Convert from m^-2 to cm^-2
         return p_area
 
-    def prob_pos(self, events):
-        def print_pos(pos):
-            print(pos.GetX(), pos.GetY(), pos.GetZ())
-
+    def get_considered_range(self, events):
         energy = events["energy"]
-        isTau = ((self.block.final_type_1 == LeptonInjector.Particle.ParticleType.TauMinus
-                 or self.block.final_type_1 == LeptonInjector.Particle.ParticleType.TauPlus)
-                 or (self.block.final_type_2 == LeptonInjector.Particle.ParticleType.TauMinus
-                 or self.block.final_type_2 == LeptonInjector.Particle.ParticleType.TauPlus))
+        zenith = events["zenith"]
+        azimuth = events["azimuth"]
+        x = events["x"]
+        y = events["y"]
+        z = events["z"]
+        isTau = ((self.block["final_type_1"] == LeptonInjector.Particle.ParticleType.TauMinus
+            or self.block["final_type_1"] == LeptonInjector.Particle.ParticleType.TauPlus)
+            or (self.block["final_type_2"] == LeptonInjector.Particle.ParticleType.TauMinus
+                or self.block["final_type_2"] == LeptonInjector.Particle.ParticleType.TauPlus))
 
         use_electron_density = LeptonInjector.getInteraction(
-            self.block["final_type_1"],
-            self.block["final_type_2"])
+                    LeptonInjector.Particle.ParticleType(self.block["final_type_1"]),
+                    LeptonInjector.Particle.ParticleType(self.block["final_type_2"]))
 
-        position = np.array([LeptonInjector.LI_Position(x, y, z) for x,y,z in zip(events["x"], events["y"], events["z"])])
-        direction = np.array([LeptonInjector.LI_Direction(zenith, azimuth) in zenith,azimuth in zip(events["zenith"], events["azimuth"])])
+        position = np.array([LeptonInjector.LI_Position(xx, yy, zz) for xx,yy,zz in zip(x,y,z)])
+        direction = np.array([LeptonInjector.LI_Direction(zen, azi) for zen,azi in zip(zenith, azimuth)])
         endcapLength = self.block["length"] * LeptonInjector.Constants.meter
 
         pca = self.get_pca(direction, position)
 
-        lepton_range = MWEtoColumnDepthCGS(GetLeptonRange(energy, isTau=isTau))
-        endcap_range = earthModel.GetColumnDepthInCGS(
+        lepton_range = self.MWEtoColumnDepthCGS(self.GetLeptonRange(energy, isTau=isTau))
+        endcap_range = self.GetColumnDepthInCGS(
                 pca - endcapLength*direction,
                 pca + endcapLength*direction,
                 use_electron_density)
 
         totalColumnDepth = lepton_range + endcap_range
 
-        maxDist = earthModel.DistanceForColumnDepthToPoint(
-            pca + endcapLength*direction,
-            direction,
-            totalColumnDepth,
-            use_electron_density) - endcapLength
+        maxDist = self.DistanceForColumnDepthToPoint(
+                pca + endcapLength*direction,
+                direction,
+                totalColumnDepth,
+                use_electron_density) - endcapLength
 
-        actualColumnDepth = earthModel.GetColumnDepthInCGS(
-            pca + endcapLength * direction,
-            pca - maxDist * direction,
-            use_electron_density)
+        actualColumnDepth = self.GetColumnDepthInCGS(
+                pca + endcapLength * direction,
+                pca - maxDist * direction,
+                use_electron_density)
 
+        mask = actualColumnDepth < (totalColumnDepth-1)
 
-        totalColumnDepth[actualColumnDepth < (totalColumnDepth-1)] = actualColumnDepth
+        totalColumnDepth[mask] = actualColumnDepth[mask]
 
         first_point = pca - maxDist * direction
         last_point = pca + endcapLength * direction
 
-        atmoEntry, atmoExit = GetAtmoPoints(pca, direction, atmoEntry, atmoExit)
+        #atmoEntry, atmoExit = self.GetAtmoPoints(pca, direction)
 
-        atmoDist = np.array([x.Magnitude() for x in (pca-atmoEntry)])
-        dist = np.array([x.Magnitude() for x in (pca - first_point)])
-        if np.abs(dist-atmoDist)<100.0:
-            dist = min(dist,atmoDist)
+        #segments = self.GetEarthDensitySegments(first_point, last_point, use_electron_density)
+        return first_point, last_point
 
-        segments = earthModel.GetEarthDensitySegments(first_point, last_point, use_electron_density)
-        #rint((first_point - last_point).Magnitude())
+    def prob_pos(self, events):
+        events = np.asarray(events)
 
-        #earthModel.GetColumnDepthInCGS
-        #earthModel.GetColumnDepthInCGS(pca-config.endcapLength*dir,pca+config.endcapLength*dir, use_electron_density)
+        #considered_range = get_considered_range(events)
+
+        energy = events["energy"]
+        zenith = events["zenith"]
+        azimuth = events["azimuth"]
+        x = events["x"]
+        y = events["y"]
+        z = events["z"]
+
+        isTau = ((self.block["final_type_1"] == LeptonInjector.Particle.ParticleType.TauMinus
+            or self.block["final_type_1"] == LeptonInjector.Particle.ParticleType.TauPlus)
+            or (self.block["final_type_2"] == LeptonInjector.Particle.ParticleType.TauMinus
+                or self.block["final_type_2"] == LeptonInjector.Particle.ParticleType.TauPlus))
+
+        use_electron_density = LeptonInjector.getInteraction(
+                    LeptonInjector.Particle.ParticleType(self.block["final_type_1"]),
+                    LeptonInjector.Particle.ParticleType(self.block["final_type_2"]))
+
+        position = np.array([LeptonInjector.LI_Position(xx, yy, zz) for xx,yy,zz in zip(x,y,z)])
+        direction = np.array([LeptonInjector.LI_Direction(zen, azi) for zen,azi in zip(zenith, azimuth)])
+        endcapLength = self.block["length"] * LeptonInjector.Constants.meter
+
+        pca = self.get_pca(direction, position)
+
+        lepton_range = self.MWEtoColumnDepthCGS(self.GetLeptonRange(energy, isTau=isTau))
+        endcap_range = self.GetColumnDepthInCGS(
+                pca - endcapLength*direction,
+                pca + endcapLength*direction,
+                use_electron_density)
+
+        totalColumnDepth = lepton_range + endcap_range
+        density = self.GetEarthDensityInCGS(position) * self.GetPNERatio(position)
+        return density / totalColumnDepth * 1e2 # Convert cm^-1 to m^-1
+
